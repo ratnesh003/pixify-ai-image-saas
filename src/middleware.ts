a49d4317823f6b050f6074d728803158c91258e5
Ratnesh Pasi <ratneshpasi03@gmail.com>
2024-07-11 03:06:27 +0530

    clerk webhook steup

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,9 +2,10 @@ import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
 
 const isPublicRoute = createRouteMatcher(
   [
+    '/',
     '/sign-in(.*)', 
     '/sign-up(.*)',
-    '/'
+    '/api/webhooks/clerk'
   ]
 );
 
