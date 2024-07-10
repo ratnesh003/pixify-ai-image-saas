aa57f40e3e9fed8648b0d223c7d70fe6591c0266
Ratnesh Pasi <ratneshpasi03@gmail.com>
2024-07-11 05:09:10 +0530

    auth redirection link updates

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,13 +4,11 @@ import "./globals.css";
 import { cn } from "@/lib/utils";
 import { ClerkProvider } from "@clerk/nextjs";
 
-const IBMPlex = IBM_Plex_Sans(
-  { 
-    subsets: ["latin"],
-    weight: ["400", "500", "600", "700"],
-    variable: "--font-ibm-plex"
-  }
-);
+const IBMPlex = IBM_Plex_Sans({
+  subsets: ["latin"],
+  weight: ["400", "500", "600", "700"],
+  variable: "--font-ibm-plex",
+});
 
 export const metadata: Metadata = {
   title: "Pixify",
@@ -23,14 +21,21 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <ClerkProvider afterSignOutUrl={"/"} appearance={{
-      variables: { colorPrimary: '#624cf5' }
-    }}>
-    <html lang="en">
-      <body className={cn("font-IBMPlex antialiased", IBMPlex.variable)}>
-        {children}
-      </body>
-    </html>
+    <ClerkProvider
+      // signInFallbackRedirectUrl={"/"}
+      // signUpFallbackRedirectUrl={"/"}
+      signInForceRedirectUrl={"/"}
+      signUpForceRedirectUrl={"/"}
+      afterSignOutUrl={"/"}
+      appearance={{
+        variables: { colorPrimary: "#624cf5" },
+      }}
+    >
+      <html lang="en">
+        <body className={cn("font-IBMPlex antialiased", IBMPlex.variable)}>
+          {children}
+        </body>
+      </html>
     </ClerkProvider>
   );
 }
